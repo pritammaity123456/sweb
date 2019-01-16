4a0a7b76cb4d4cff2a4905affdfd331a9bb6c9c0
opentech4u@gmail.com <opentech4u@gmail.com>
2019-01-16 10:26:20 +0530

    rectification16119

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,14 +1,10 @@
-import { HostListener, Component, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-@HostListener('window:scroll', ['$event']) 
-    scrollHandler(event) {
-      console.debug("Scroll Event");
-    }
 export class HeaderComponent implements OnInit {
 
   constructor() { }
