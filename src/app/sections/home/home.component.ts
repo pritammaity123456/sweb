f813b94a3751932b05842a3cbb16ab79ec3f8ba1
opentech4u@gmail.com <opentech4u@gmail.com>
2019-01-17 18:57:22 +0530

    header modified again 17119

--- src/app/sections/home/home.component.ts
+++ src/app/sections/home/home.component.ts
@@ -1,29 +1,36 @@
 import { Component } from '@angular/core';
+import { NgbCarouselConfig } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'ngbd-carousel-basic',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
+  providers: [NgbCarouselConfig]
 })
 export class HomeComponent {
+
+  showNavigationArrows = false;
+  showNavigationIndicators = false;
 
   images:any = [
 
     {
-      head: 'IMG 1',
-      description: 'Description',
+      head: 'Get on the right track with us',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec vulputate felis at risus blandit placerat. Mauris convallis, ligula eu lobortis elementum, diam neque eleifend ipsum, et consequat neque est non nibh. Mauris viverra pharetra mi, ',
       path: '../../../assets/img1.jpg'
     },
     {
-      head: 'IMG 2',
-      description: 'Description',
+      head: 'We know your project\'s value',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec vulputate felis at risus blandit placerat. Mauris convallis, ligula eu lobortis elementum, diam neque eleifend ipsum, et consequat neque est non nibh. Mauris viverra pharetra mi, ',
       path: '../../../assets/img2.jpg'
     },
     {
-      head: 'IMG 3',
-      description: 'Description',
+      head: 'Looking Forward for mobile application',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec vulputate felis at risus blandit placerat. Mauris convallis, ligula eu lobortis elementum, diam neque eleifend ipsum, et consequat neque est non nibh. Mauris viverra pharetra mi, ',
       path: '../../../assets/img3.jpg'
     }
 
   ];
+
+
 }
